666a7941c1f65e7e4370adfa1d1fa9e949b20e7f
Nasser Issa <nasissa97@gmail.com>
2020-08-28 00:33:23 -0700

    the code

---
+++ curr-app/src/containers/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import Persons from '../components/Persons/Persons';
+import classes from './App.css';
+import Person from '../components/Persons/Person/Person';
+import Cockpit from '../components/Cockpit/Cockpit';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
+  state = {
+    persons: [
+      { id: 'dfkjl', name: 'Nas', age: 28},
+      { id: 'djke',  name: 'Mom', age: 24},
+      { id: 'nekl3', name: 'Pablo', age:14}
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+  
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMunt')
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} ) 
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex,1);
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+
+  }
+
+  render() {
+    console.log('[App.js] render');
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler} />;
+    }
+
+    return (
+      <div className={classes.App}>
+        <Cockpit 
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons} 
+          clicked={this.togglePersonsHandler} />
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
